cc328a05fe633149cc4d6c663d0caff7dbfa7b77
JB <jbrilla17@gmail.com>
2015-09-05 16:55:36 +1000

    adding total points logic

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -3,7 +3,34 @@ class PointsController < ApplicationController
   	@points = Point.all
   	@activities = Activity.all
   	@users = User.all
+
+  	#Gets All Activity IDs
+  	@activity_ids = []
+  	@activities.each do |id|
+  		@activity_ids << id.id
+  	end
+
+  	#Get total points for each activity
+  	@point_totals = {}
+	@activity_ids.each do |x|
+		p = Point.where(:activity_id => x)
+		@point_totals[x] = 0
+		p.each do |y|
+			@point_totals[x] += y.points
+		end
+	end
+
+	#Gets Users of each activity
+	# @activity_users = []
+	# @activity_ids.each do |id|
+	# 	@activity_users << id.user_id
+	# end
+
+
+
+
   end
+
 
   def show
   end
@@ -13,4 +40,5 @@ class PointsController < ApplicationController
 
   def edit
   end
+
 end
