7a7f39b2ceb1ec6e91c2d68c1f17acde5d6eb1b0
JB <jbrilla17@gmail.com>
2015-09-05 18:02:36 +1000

    adding create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,24 @@ class UsersController < ApplicationController
   end
 
   def show
+  	@user = User.find params[:id]
   end
 
   def new
+  	@user = User.new
+  end
+
+  def create
+  	user = User.create user_params
+	redirect_to user
   end
 
   def edit
+  end
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:email, :name)
   end
 end
