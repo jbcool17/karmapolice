d75967c54333d06529db629a98c7470d0d3c2ec8
John Brilla <jbrilla17@gmail.com>
2015-09-11 07:02:04 +1000

    images fix/zindex

--- app/assets/javascripts/code.js
+++ app/assets/javascripts/code.js
@@ -9,10 +9,14 @@ $(document).ready(function() {
 	//REMOVE BoxShadow From Slider
 	$('.bx-wrapper .bx-viewport').css('box-shadow', '0 0 5px white');
 	$('.bx-wrapper .bx-viewport').css('text-align', 'center');
+
 	
 	//Center Image in Slider
+	$('img').css('border-radius', '20px');
 	$('img').css('margin-left', 'auto');
 	$('img').css('margin-right', 'auto');
+	$('img').css('margin-top', 'auto');
+	$('img').css('margin-bottom', 'auto');
 
     console.log("loaded-code")
 
